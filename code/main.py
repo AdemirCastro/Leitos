86150977d13cb9e21c63c2dfc8f55c0fa393c07a
Ademir Castro <oademircastro@gmail.com>
2022-10-09 14:37:55 -0300

    aesthetic ajustments on main.py

--- code/main.py
+++ code/main.py
@@ -2,11 +2,11 @@ from beds import bed_tab_by_uf, brazil_bed_tab
 from time import time
 import os
 start_time = time()
-################# Execute as funções aqui ################
+################# Run your code here ################
 
 brazil_bed_tab()
 
-##########################################################
+#####################################################
 end_time = time()
 execution_time = end_time-start_time
 print(f"""Program executed in {int(execution_time//3600)} hours, 
